01e9c4c74e7d4582f79b4e2792ca26840c3a42b3
ruamess <zlaptiev@gmail.com>
2025-03-09 05:39:30 +0500

    add height to logo

--- src/modules/header/components/Logo.tsx
+++ src/modules/header/components/Logo.tsx
@@ -1,7 +1,7 @@
 import { memo } from "react"
 
 const Logo = () => {
-  return <img src="logo.svg" alt="Match Tracker" className="w-56 lg:w-64" />
+  return <img src="logo.svg" alt="Match Tracker" className="w-56 h-7 lg:w-64 lg:h-8" />
 }
 
 export default memo(Logo)
