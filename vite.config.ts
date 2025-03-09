656dddcca669f9cdc1711fa87dabc993cff8dcba
ruamess <zlaptiev@gmail.com>
2025-03-09 16:14:32 +0500

    improve Vite config structure and chunk splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { visualizer } from "rollup-plugin-visualizer"
 import { fileURLToPath } from "url"
 import path from "path"
 
@@ -7,12 +8,33 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: false,
+      gzipSize: true
+    })
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+
+          if (id.includes("node_modules")) return "react"
+          if (id.includes("src/components/")) return "components"
+          if (id.includes("src/modules/")) return "modules"
+          if (id.includes("src/shared/")) return "shared"
+
+          return null
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "src/components"),
       "@modules": path.resolve(__dirname, "src/modules"),
       "@shared": path.resolve(__dirname, "src/shared"),
-    },
-  },
+    }
+  }
 })
